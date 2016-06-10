80bb13f1d3633d5d15da6ba6034111980d0b4f98
jrbenny35 <bennyjr169@gmail.com>
2016-06-10 18:33:00 -0400

    Removed some fixtures and hopefully cleaned it up

--- og_main_test.py
+++ og_main_test.py
@@ -1,11 +1,38 @@
 import pytest
+import requests
+from bs4 import BeautifulSoup
 from test_opengraph_standards import TestOpenGraphStandards
 
-def test_it():
+og_required = [
+    'og:title',
+    'og:type',
+    'og:url',
+    'og:image',
+]
+
+og_found = []
+
+def test():
+    # Instansiate object
     test = TestOpenGraphStandards() #Instansiate object
 
-    #Pass in URL
-    data = test.get_url("http://ogp.me/")
+    # Pass in URL
+    data = test.get_meta("http://nightly.mozilla.org")
 
-    #pass url and test
-    test.test_for_og_standards(data)
+    #Test list and add found tags to list
+    for og_type in og_required:
+        for meta in data:
+            if meta.get('property') == og_type:
+                og_found.append(og_type)
+            else:
+                continue
+
+    # Final assert testing the 2 lists
+    for item in og_required:
+        if item in og_found:
+            og_assert = True
+            continue
+        else:
+            print "Item %s not found" % item
+            og_assert = False
+    assert og_assert
--- test_opengraph_standards.py
+++ test_opengraph_standards.py
@@ -5,40 +5,9 @@ from bs4 import BeautifulSoup
 
 class TestOpenGraphStandards:
 
-    # Required og tags
-    og_required = [
-        'og:title',
-        'og:type',
-        'og:url',
-        'og:image',
-    ]
-
-    # Stors og tags found on page
-    og_found = []
-
-    # Test if the page has required tags
+    # Load url and parse it for meta tags
     @pytest.fixture
-    def test_for_og_standards(self, url):
-        for og_type in self.og_required:
-            for meta in url:
-                if meta.get('property') == og_type:
-                    self.og_found.append(og_type)
-                else:
-                    continue
-
-        # Run test against requred list every time
-        self.test_required()
-
-    # Test if the tags found match required tags
-    @pytest.fixture
-    def test_required(self):
-        for item in self.og_required:
-            assert item in self.og_found, "Item: {} was not found".format(item)
-            return True
-
-    # Load url and parse it
-    @pytest.fixture
-    def get_url(self, url):
+    def get_meta(self, url):
         og_site = requests.get(url)
         soup = BeautifulSoup(og_site.content, "html.parser")
         return soup.find_all("meta")
