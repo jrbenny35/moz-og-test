6ac479b20001f96f565a1f303ce2cd23af7606a0
jrbenny35 <bennyjr169@gmail.com>
2016-06-14 13:22:14 +0100

    Revert to working code

--- og_main_test.py
+++ og_main_test.py
@@ -1,21 +1,10 @@
 import pytest
-import requests
-from bs4 import BeautifulSoup
 from test_opengraph_standards import TestOpenGraphStandards
 
-og_required = [
-    'og:title',
-    'og:type',
-    'og:url',
-    'og:image',
-]
-
-og_found = []
-
-def test():
-    # Instansiate object
+def test_it():
     test = TestOpenGraphStandards() #Instansiate object
 
+<<<<<<< HEAD
     # Pass in URL
     data = test.get_meta("http://ogp.me/")
     count = 0
