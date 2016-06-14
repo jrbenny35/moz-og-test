46835612bb67907cf5792b929e6dc371e4c46564
jrbenny35 <bennyjr169@gmail.com>
2016-06-14 13:20:58 +0100

    An attempt at a logic path I had

--- og_main_test.py
+++ og_main_test.py
@@ -17,22 +17,27 @@ def test():
     test = TestOpenGraphStandards() #Instansiate object
 
     # Pass in URL
-    data = test.get_meta("http://nightly.mozilla.org")
-
+    data = test.get_meta("http://ogp.me/")
+    count = 0
     #Test list and add found tags to list
     for og_type in og_required:
         for meta in data:
             if meta.get('property') == og_type:
+                count + 1
                 og_found.append(og_type)
+                break
             else:
+                print "Item {} not found.".format(og_type)
                 continue
+        assert count == 4
+
 
     # Final assert testing the 2 lists
-    for item in og_required:
-        if item in og_found:
-            og_assert = True
-            continue
-        else:
-            print "Item %s not found" % item
-            og_assert = False
-    assert og_assert
+    #for item in og_required:
+        #if item in og_found:
+        #    og_assert = True
+        #    break
+        #else:
+        #    print "Item {} not found".format(item)
+        #    og_assert = False
+    #assert og_assert
